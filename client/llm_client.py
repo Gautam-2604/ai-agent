cb05a7c2548f110690207b018daf1195bd510ecf
Gautam-2604 <gaurisariag@gmail.com>
2026-01-10 20:47:38 +0530

    interfacing done

--- client/llm_client.py
+++ client/llm_client.py
@@ -7,7 +7,7 @@ class LLMClient:
     def get_client(self)->AsyncOpenAI:
         if self._client is None:
             self._client = AsyncOpenAI(
-                api_key='',
+                api_key='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',
                 base_url='https://openrouter.ai/api/v1'
             )
             return self._client
@@ -20,14 +20,14 @@ class LLMClient:
     async def chat_completions(self, messages: list[dict[str, any]], stream: bool=True):
         client = self.get_client()
         kwargs = {
-            "model":"",
+            "model":"arcee-ai/trinity-mini:free",
             "messages":messages,
             "stream":stream
         }
         if stream:  
-            self._stream_response()
+            await self._stream_response()
         else:
-            self._non_stream_response()
+            await self._non_stream_response(client, kwargs)
 
     async def _stream_response(self):
         pass
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from client.llm_client import LLMClient
+import asyncio
+async def main():
+    client = LLMClient()
+    messages = [{
+        "role":"user",
+        "content":"Hi, Whats up"
+    }]
+    await client.chat_completions(messages, False)
+
+asyncio.run(main())
